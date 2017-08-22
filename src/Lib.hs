1d209532e526bfd7d59ddf93e25ff3fbb58d1c90
Alexander Shabalin <shabalyn.a@gmail.com>
2017-08-22 21:19:48 +0300

    Change structure again

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,27 +1,38 @@
 module Lib (someFunc) where
 
+import qualified Data.List as List
+
 type Value = Int
 type Position = (Int, Int)
 data CellData
   = CellValue Value
   | CellAvailability [Value]
 type Cell = (Position, CellData)
-type Grid = [[Cell]]
+type Grid = [Cell]
+
+horizontalContextFor :: Grid -> Position -> [Cell]
+horizontalContextFor grid (x,y) = [cell | cell@((xc,yc),_) <- grid, xc == x, yc /= y]
+
+verticalContextFor :: Grid -> Position -> [Cell]
+verticalContextFor grid (x,y) = [cell | cell@((xc,yc),_) <- grid, xc /= x, yc == y]
+
+cellContextFor :: Grid -> Position -> [Cell]
+cellContextFor grid (x,y) = [cell | cell@((xc,yc),_) <- grid, (yc /= y || xc /= x), (x `div` 3 == xc `div` 3), (y `div` 3 == yc `div` 3)]
 
 printGrid :: Grid -> String
-printGrid = unlines . map (concatMap go)
+printGrid = unlines . map (concatMap go) . List.groupBy (\((_,pl),_) ((_,pr),_) -> pl == pr)
  where go :: Cell -> String
        go (_, CellValue v) = show v
        go (_, CellAvailability _) = "."
 
 printAvailability :: Grid -> String
-printAvailability = concatMap (unlines . filter (not . null) . map go)
+printAvailability = unlines . filter (not . null) . map go
  where go :: Cell -> String
        go (_, CellValue _) = ""
        go (p, CellAvailability vals) = show p ++ ": " ++ show vals
 
 readGrid :: String -> Grid
-readGrid = zipWith (\i -> zipWith (\j c -> ((i, j), go c)) [0..]) [0..] . lines
+readGrid = concat . zipWith (\i -> zipWith (\j c -> ((j, i), go c)) [0..]) [0..] . lines
  where go :: Char -> CellData
        go '.' = CellAvailability [1..9]
        go c
